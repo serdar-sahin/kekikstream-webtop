4973f6344f27be155647273322e1ddb1da6e5217
serdar-sahin <serdar.umut@gmail.com>
2025-03-02 16:27:24 +0300

    pagination - sayfalama özelliği eklendi

--- src/KekikStream.Webtop.Blazor/Pages/Index.razor.cs
+++ src/KekikStream.Webtop.Blazor/Pages/Index.razor.cs
@@ -2,6 +2,7 @@
 using KekikStream.Webtop.Medias;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.JSInterop;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -18,6 +19,11 @@ public partial class Index
 
     private List<SearchResult>? searchResults;
     private string? query;
+
+    private bool isPagination = false;
+    private int currentPageNumber = 1;
+    private string currentCategoryName = "";
+    private string currentCategoryUrl = "";
 
     private bool isBusy = false;
     private bool isWarning = false;
@@ -59,6 +65,7 @@ public partial class Index
         pages = null;
         searchResults = null;
         pageName = "";
+        currentPageNumber = 1;
 
         plugin = await mediaService.GetPluginAsync(name);
 
@@ -68,15 +75,21 @@ public partial class Index
             {
                 var firstPage = plugin.MainPage[0];
                 pageName = firstPage.Title;
+                currentCategoryName = firstPage.Title;
+                currentCategoryUrl = firstPage.Url;
+
                 pages = await mediaService.GetMainPageAsync(plugin.Name, 1, firstPage.Url, firstPage.Title);
                 searchResults=  ObjectMapper.Map(pages, searchResults);
                 //Debug.WriteLine("results: " + searchResults?.ToJson());
+
+                isPagination = true;
             }
             
             //Debug.WriteLine("Plugin: " + plugin.ToJson());
         }
         else
         {
+            isPagination = false;
             ShowInfo(false);
         }
 
@@ -90,6 +103,8 @@ public partial class Index
         pages = null;
         searchResults = null;
         pageName = "";
+        currentCategoryName = "";
+        currentCategoryUrl = "";
 
         if (plugin != null)
         {
@@ -98,11 +113,69 @@ public partial class Index
 
             if (pages != null)
             {
+                currentCategoryName = name;
+                currentCategoryUrl = url;
                 searchResults = ObjectMapper.Map(pages, searchResults);
+                //Debug.WriteLine("results: " + searchResults?.ToJson());
+
+                isPagination = true;
+            }
+            else
+            {
+                isPagination = false;
+                ShowInfo(false);
+                //Debug.WriteLine("Pages: " + pages.ToJson());
+            }
+        }
+
+        await js.InvokeVoidAsync("backToTop");
+        isBusy = false;
+    }
+
+    private async Task NextPages(string direction)
+    {
+        if (currentCategoryName == "" && currentCategoryUrl == "")
+        {
+            return;
+        }
+
+        if (direction == "Prev" && currentPageNumber == 1)
+        {
+            return;
+        }
+
+        isBusy = true;
+        pages = null;
+        searchResults = null;
+        pageName = "";
+
+        if (plugin != null)
+        {
+            pageName = currentCategoryName;
+            pages = await mediaService.GetMainPageAsync(plugin.Name, currentPageNumber, currentCategoryUrl, currentCategoryName);
+
+            if (pages != null)
+            {
+                searchResults = ObjectMapper.Map(pages, searchResults);
+
+                if(direction == "Next")
+                {
+                    currentPageNumber++;
+                }
+                else
+                {
+                    currentPageNumber--;
+                }
+
+                if(currentPageNumber <= 0)
+                {
+                    currentPageNumber = 1;
+                }
                 //Debug.WriteLine("results: " + searchResults?.ToJson());
             }
             else
             {
+                isPagination = false;
                 ShowInfo(false);
                 //Debug.WriteLine("Pages: " + pages.ToJson());
             }
@@ -115,9 +188,12 @@ public partial class Index
     private async Task Search()
     {
         isBusy = true;
+        isPagination = false;
         pages = null;
         searchResults = null;
         pageName = L["SearchResults"];
+        currentCategoryName = "";
+        currentCategoryUrl = "";
 
         if (plugin != null && query != null)
         {
